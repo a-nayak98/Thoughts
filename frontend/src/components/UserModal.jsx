ef306a712e50938220c8714a8ba33ffbd8a575ce
unknown <nabhi977756@gmail.com>
2023-04-30 09:44:16 +0530

    Made google client id visible

--- frontend/src/components/UserModal.jsx
+++ frontend/src/components/UserModal.jsx
@@ -6,7 +6,9 @@ import { ModalContext } from "../context/ModalContext";
 
 const UserModal = () => {
   const { show, handleClose } = useContext(ModalContext);
-  const GoogleClientUrl = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+  // const GoogleClientUrl = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+  const GoogleClientUrl =
+    "402038778481-qterqdcdc42fc2h6qqkpsbn9q7monqil.apps.googleusercontent.com";
 
   return (
     <Modal show={show} onHide={handleClose} keyboard={false} centered>
